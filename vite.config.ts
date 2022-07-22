70b9a56f507795f0e774a3e924f7cff7696de2da
Barnaby Collins <barnabyc@sky.com>
2022-07-22 18:03:00 +0100

    Use subdomain instead of subdirectory

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    base: "/S2Z-Challenge/"
-  }
+  plugins: [react()]
 })
